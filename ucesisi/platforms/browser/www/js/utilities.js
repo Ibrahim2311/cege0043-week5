5eb6b44e5e1d59649005d0eaf116ccb314a292c2
ibrahim2311 <i.s.h.94@hotmail.com>
2019-03-09 09:45:13 -0500

    adding div to be changed by ajax request

--- ucesisi/platforms/browser/www/js/utilities.js
+++ ucesisi/platforms/browser/www/js/utilities.js
@@ -5,9 +5,9 @@ function getPort(){
     xhr.addEventListener("load", function () {
         var parser = new DOMParser();
         var doc = parser.parseFromString(xhr.responseText, "application/xml");
-        httpPortNumber= doc.getElementsByTagName("node-porthttp").item(0).textContent;
-        httpsPortNumber= doc.getElementsByTagName("node-porthttps").item(0).textContent;
-        alert("Port : " + httpPortNumber);
+        httpPortNumber= doc.getElementsByTagName("node-port-http").item(0).textContent;
+        httpsPortNumber= doc.getElementsByTagName("node-port-https").item(0).textContent;
+        // alert("Port : " + httpPortNumber);
     });
     // depending on whether we are in a browser or on a phone
     // the location of the config file is different
