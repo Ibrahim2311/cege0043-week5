4c9ff5f020e92dbf941870728d52c8c033a2e699
ibrahim2311 <i.s.h.94@hotmail.com>
2019-03-10 16:54:50 -0400

    using loadw3 to load html template into div

--- ucesisi/platforms/browser/www/js/startup.js
+++ ucesisi/platforms/browser/www/js/startup.js
@@ -2,10 +2,15 @@ function trackAndCircle() {
 	addPointLinePoly()
 	getEarthquakes()
 	getPort()
+	loadW3HTML()
 };
 
 function startup() {
 	document.addEventListener('DOMContentLoaded', function() {
 		trackAndCircle();
 	}, false);
+}
+
+function loadW3HTML() {
+	w3.includeHTML();
 }
